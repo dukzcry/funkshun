fb6ed505461d1736bfeef19b8fec93e16ea25847
A. V. Lukyanov <artem.falcon@gmail.com>
2013-06-01 00:24:18 +0400

    Bring back timeouts.

--- ipxerlay/run.sh
+++ ipxerlay/run.sh
@@ -5,6 +5,7 @@ if [ "$1" == "compile" ]; then
 	#erl -pa . -s ipxerlay test_call -s erlang halt
 fi
 
+# to = allowed idle timeout in msecs
 # port = portnum | 0 for random
-# (port inet|inet6 addr) | (port fd)
-erl -pa . -s ipxerlay start_link 4899 inet 192.168.1.2
+# (to port inet|inet6 addr) | (to port fd)
+erl -pa . -s ipxerlay start_link 900000 4899 inet 192.168.1.2
